fdb6895f412637c11aa4826b39d17f6ad957997d
anton_kuklin <homyak91@inbox.ru>
2022-11-22 23:42:05 +0400

    add new scripts

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 document.addEventListener('DOMContentLoaded' , () => {
+
+  let checkboxYes = document.querySelector('.js-checkbox-yes');
+  let checkboxNo = document.querySelector('.js-checkbox-no');
+  let step1 = document.querySelector('.js-step-1');
+  let step2 = document.querySelector('.js-step-2');
+  let checkboxImg = document.querySelector('.js-img-yes');
+  let checkboxImgNo = document.querySelector('.js-img-no');
+  let flag = 0;
+
+  checkboxYes.addEventListener('click', () => {
+    checkboxImg.classList.add('checked');
+    setTimeout(function() {
+      step1.classList.add('hide')
+      step2.classList.remove('hide')
+    }, 3000)
+  })
+
+  checkboxImgNo.addEventListener('mouseover', () => {
+    flag += 1;
+    if (flag % 2 === 0) {
+      checkboxImgNo.style.transform = "translateX(170%)";
+    } else {
+      checkboxImgNo.style.transform = "translateY(200%)";
+    }
+    if (flag === 5) {
+      checkboxImgNo.style.transform = "translate(0, 0)";
+      flag = 0;
+    }
+  })
+
+
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         direction: 'horizontal',
